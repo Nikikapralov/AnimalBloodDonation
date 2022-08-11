d5a94011fa3eb3cba131d62404cf9b80008b9a97
Nikikapralov <nikikapralov1@gmail.com>
2022-08-11 04:54:23 +0200

    added the current models

--- Backend/Backend/AnimalBloodDonationBE/models.py
+++ Backend/Backend/AnimalBloodDonationBE/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    is_deleted = models.BooleanField(default=False)
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50, default="Animal")
+    age = models.IntegerField()
+    photo = models.ImageField(upload_to="pet_images/")
+    breed = models.CharField(max_length=50, default="Unknown")
+    blood_type = models.CharField(max_length=10, default="Unknown")
+    region = models.CharField(max_length=50, default="Not specified")
+    description = models.CharField(max_length=500)
+    is_deleted = models.BooleanField(default=False)
+    owner_id_fk = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+
+class Case(models.Model):
+    request_blood = models.BooleanField(default=False)
+    donate_blood = models.BooleanField(default=True)
+    description = models.CharField(max_length=500)
+    pet_id_fk = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(default=False)
+
+
+class CaseImage(models.Model):
+    case_id_fk = models.ForeignKey(Case, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="case_images/")
+    is_deleted = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    text_comment = models.CharField(max_length=500)
+    is_deleted = models.BooleanField(default=False)
+    case_id_fk = models.ForeignKey(Case, on_delete=models.CASCADE)
+
+
+class Message(models.Model):
+    text_message = models.CharField(max_length=500)
+
+
+class PrivateMessageTable(models.Model):
+    sender_id_fk = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    recipient_id_fk = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    message_id_fk = models.ForeignKey(Message, on_delete=models.CASCADE)
+
--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Backend.AnimalBloodDonationBE',
+    'Backend.auth'
 ]
 
 ROOT_URLCONF = 'Backend.urls'
